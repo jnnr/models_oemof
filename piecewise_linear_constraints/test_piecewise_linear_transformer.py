a6d7347078b96dee52d6064aad990d20457bd373
jnnr <32454596+jnnr@users.noreply.github.com>
2019-06-19 18:23:23 +0200

    Correct api and plot

--- piecewise_linear_constraints/test_piecewise_linear_transformer.py
+++ piecewise_linear_constraints/test_piecewise_linear_transformer.py
@@ -11,8 +11,8 @@ solver = 'cbc'
 # set timeindex and create data
 periods = 20
 datetimeindex = pd.date_range('1/1/2019', periods=periods, freq='H')
-x = np.arange(0, periods, 1)
-demand = 50 * np.sin(0.8*x) + 50
+step = 5
+demand = np.arange(0, step*periods, step)
 
 # set up EnergySystem
 energysystem = EnergySystem(timeindex=datetimeindex)
@@ -23,17 +23,15 @@ energysystem.add(Sink(label='demand', inputs={b_el: Flow(
     nominal_value=1, actual_value=demand, fixed=True)}))
 
 conv_func = lambda x: 0.01 * x**2
-in_breakpoints = np.arange(0, 150, 30)
-out_breakpoints = conv_func(in_breakpoints)
+in_breakpoints = np.arange(0, 110, 25)
 
 pwltf = solph.custom.PiecewiseLinearTransformer(
     label='pwltf',
     inputs={b_gas: solph.Flow(
-    nominal_value=220,
+    nominal_value=100,
     variable_costs=1)},
     outputs={b_el: solph.Flow()},
     in_breakpoints=in_breakpoints,
-    out_breakpoints=out_breakpoints,
     conversion_function=conv_func,
     pw_repn='CC')
 
@@ -50,7 +48,20 @@ string_results = outputlib.processing.convert_keys_to_strings(results)
 sequences = {k:v['sequences'] for k, v in string_results.items()}
 df = pd.concat(sequences, axis=1)
 df[('efficiency', None, None)] = df[('pwltf', 'electricity')].divide(df[('gas', 'pwltf')])
-print(df)
+
+def linearized_func(func, x_break, x):
+    y_break=func(x_break)
+    condlist = [(x_l <= x)&(x < x_u) for x_l, x_u in zip(x_break[:-1], x_break[1:])]
+    funclist = []
+    for i in range(len(x_break)-1):
+        b = y_break[i]
+        a = (y_break[i+1] - y_break[i]) * 1/((x_break[i+1] - x_break[i]))
+        funclist.append(lambda x, b=b, a=a, i=i: b + a*(x-x_break[i]))
+    return np.piecewise(x, condlist, funclist)
+
+production_expected = linearized_func(conv_func, in_breakpoints, df[('gas', 'pwltf')]['flow'].values)
+production_modeled = df[('pwltf', 'electricity')]['flow'].values
+print(np.allclose(production_modeled, production_expected))
 
 fig, ax = plt.subplots()
 ax.scatter(df[('gas', 'pwltf')], df[('pwltf', 'electricity')], marker='x', c='r', s=40)
